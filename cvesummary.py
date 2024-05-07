01bf3343ad26001b20d51824192781ecd058c474
Pali Surdhar Local <pali.surdhar@entrust.com>
2024-05-07 13:19:13 +0100

    fixed so that descriptions are listed

--- cvesummary.py
+++ cvesummary.py
@@ -1,4 +1,14 @@
 import openpyxl
+import nvdlib
+
+def read_cves_from_textfile(filename):
+    cve_list = [] 
+    with open(filename, 'r') as file:
+        for line in file:
+            cve_list.append(line.strip())
+    return cve_list
+
+
 
 def read_cves_from_excel(excel_file):
     cve_list = []
@@ -11,27 +21,35 @@ def read_cves_from_excel(excel_file):
 def get_cve_details(cve_id):
     try:
         result = nvdlib.searchCVE(cveId=cve_id)[0]
-        severity = result.v31severity
-        score = result.v31score
+        #severity = result.v30severity
+        score = result.score
         description = result.descriptions[0].value
-        vector = result.v31vector
-        return severity, score, description, vector
+        #vector = result.v30vector
+        return score, description
+#        return severity, score, description, vector
     except IndexError:
         return None, None, None, None
 
 def main():
     # Initialize CVE list
-    cve_list = ['CVE-2023-48795', 'CVE-2023-46445', 'CVE-2023-46446']
+    #cve_list = ['CVE-2024-21892', 'CVE-2023-41993', 'CVE-2024-20954', 'CVE-2024-21098', 'CVE-2024-21085', 'CVE-2024-21011',  'CVE-2024-21068','CVE-2024-21094','CVE-2024-21012','CVE-2024-21003','CVE-2024-21004','CVE-2024-21002']
+    cve_list = read_cves_from_textfile('cves.txt')
 
     for cve_id in cve_list:
-        severity, score, description, vector = get_cve_details(cve_id)
-        if severity:
+        #severity, score, description, vector = get_cve_details(cve_id)
+        score, description = get_cve_details(cve_id)  
+        if score:
+            print(f"CVE ID: {cve_id}")
+            print (f"Score: {score}")
+            print(f"Description: {description}")
+            
+        '''if severity:
             print(f"CVE ID: {cve_id}")
             print(f"Severity: {severity}")
             print(f"Score: {score}")
             print(f"Description: {description}")
             print(f"Vector: {vector}")
             print()
-
+'''
 if __name__ == "__main__":
     main()
